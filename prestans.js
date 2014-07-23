faf54495eebc54aecb83de614fb357ba202c03b6
Bradley Mclain <brad@mclain.com.au>
2014-07-23 16:26:22 +1000

    better comments for previous fix

--- prestans.js
+++ prestans.js
@@ -1,5 +1,12 @@
 goog.provide('prestans');
 
+/**
+ * Base namespace for the prestans library.  Checks to see prestans is already
+ * defined in the current scope before assigning to prevent clobbering if
+ * prestans.js is loaded more than once.
+ *
+ * @const
+ */
 var prestans = prestans || {};
 
 /**
