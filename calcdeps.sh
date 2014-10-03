99839ea3a088a474a9735e87c6b65a670338bc85
Bradley Mclain <brad@mclain.com.au>
2014-10-03 15:41:46 +1000

    renamed deps file

--- calcdeps.sh
+++ calcdeps.sh
@@ -2,4 +2,4 @@
 
 DEPSWRITER=../closure-library/closure/bin/build/depswriter.py
 
-python $DEPSWRITER --root_with_prefix=". ../../../prestans/" > deps_test.js
+python $DEPSWRITER --root_with_prefix=". ../../../prestans/" > deps.js
